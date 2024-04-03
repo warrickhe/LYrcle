95ee632f2d63f5d330a283b0fd3b365735eaa091
warrickhe <43077069+warrickhe@users.noreply.github.com>
2024-04-02 17:32:02 -0700

    Add Todos For Translator

---
+++ server/translator.py
@@ -0,0 +1,8 @@
+import pinyin #https://pypi.org/project/pinyin/ to translate from chinese to pinyin and to english
+
+'''
+things to do:
+no translation on the english
+filter out all tags as well as the mojim.com thing
+extract properly
+'''
